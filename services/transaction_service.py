5bfdc56e634a6433e884243c57589833d417540f
laharitummala-tech <laharitummala@gmail.com>
2025-12-29 21:27:10 +0530

    Realised borrowed_books not needed in users.json, can get from transaction and transaction will be completed from ISSUED TO RETURNED

--- services/transaction_service.py
+++ services/transaction_service.py
@@ -9,25 +9,26 @@ BORROW_DAYS = 14
 FINE_PER_DAY = 2
 
 def issue_book(user,book_id):
-    users_data = load_user(USERS_PATH)
-    users = users_data.get("users",[])
+    transactions_data = load_user(TRANSACTION_PATH)
+    transactions = transactions_data.get("transactions",[])
     books_data = load_user(BOOKS_PATH)
     books = books_data.get("books",[])
     
-    # find logged_in user
-    for i in users:
-        if i["user_id"] == user["user_id"]:
-            current_user = i
-            break
-    else:
-        print("user not found")
-        return
-    borrowed = current_user.get("borrowed_books",[])
+    active_borrows = [
+        t for t in transactions
+        if t["user_id"] == user["user_id"] and t["status"] == "ISSUED"
+    ]
    
     # limit check
-    if len(borrowed) >= MAX_BORROW_LIMIT:
+    if len(active_borrows) >= MAX_BORROW_LIMIT:
         print("Borrow limit reached, you can borrow only 3 books.")
         return
+    
+    # check if same book already borrowed
+    for t in active_borrows:
+        if t["book_id"] == book_id:
+            print("You already borrowed this book")
+            return
     
     # find book
     for book in books:
@@ -36,22 +37,26 @@ def issue_book(user,book_id):
                 print('Book not available right now')
                 return
             
-            if book_id in borrowed:
-                print("You already borrowed this book")
-                return
-                
-            # TRANSACTION
             book["available_copies"] -= 1
             book["borrow_count"] += 1
-            borrowed.append({
-                "book_id" : book_id,
-                "borrow_date" : str(date.today()),
-                "due_date" :str(date.today() + timedelta(days=BORROW_DAYS))
-            })
+
+            new_id = transactions[-1]["transaction_id"] + 1 if transactions else 1
+            # TRANSACTION
+            transactions.append(
+                Transaction(
+                    transaction_id=new_id,
+                    user_id=user["user_id"],
+                    book_id=book_id,
+                    issue_date=str(date.today()),
+                    due_date= str(date.today() + timedelta(days=BORROW_DAYS)),
+                    return_date=None,
+                    status="ISSUED",
+                    fine_amount=0,
+                    fine_paid=False
+                ).to_dict()    
+            )
             
-            current_user["borrowed_books"] = borrowed
-            
-            save_json(USERS_PATH,users_data)
+            save_json(TRANSACTION_PATH,transactions_data)
             save_json(BOOKS_PATH,books_data)
             
             print(f"Book '{book['title']}' borrowed successfully.")
@@ -61,30 +66,24 @@ def issue_book(user,book_id):
     
 #-------------------------------------------------------------------------------------------------------------------------------#
 def view_borrowed_books(user):
-    users_data = load_user(USERS_PATH)
-    users = users_data.get("users",[])
-    books_data = load_user(BOOKS_PATH)
-    books = books_data.get("books",[])
+    # transactions_data = load_user(TRANSACTION_PATH)
+    transactions = load_user(TRANSACTION_PATH).get("transactions",[])
+    # books_data = load_user(BOOKS_PATH)
+    books = load_user(BOOKS_PATH).get("books",[])
     
-    borrowed_books = []
-    for i in users:
-        if i["user_id"] == user["user_id"]:
-            borrowed_books = i.get("borrowed_books",[])
-            break
-        
-    else:
-        print("User not found")
+    active = [
+        t for t in transactions
+        if t["user_id"] == user["user_id"] and t["status"] == "ISSUED"
+    ]
+    if not active:
+        print("No books borrowed.")
         return
     
-    if not borrowed_books:
-        print("No books borrowed")
-        return
-    
-    print("\n--- Your borrowed books")
-    for b in borrowed_books:
+    print("\n ---Your Borrowed books---")
+    for t in active:
         for book in books:
-            if book["book_id"] == b["book_id"]:
-                print(f"ID: {book['book_id']} | {book['title']} by {book['author']} | Due : {b['due_date']}")
+            if book["book_id"] == t["book_id"]:
+                print(f"ID: {book['book_id']} | {book['title']} by {book['author']} | Due : {t['due_date']}")
 
 #-------------------------------------------------------------------------------------------------------------------------------#
 
@@ -93,25 +92,23 @@ def view_borrow_history():
     users = users_data.get("users",[])
     books_data = load_user(BOOKS_PATH)
     books = books_data.get("books",[])
-    if not users:
-        print("No users found ")
+    transactions = load_user(TRANSACTION_PATH).get("transactions",[])
+    if not transactions:
+        print("No Transactions found ")
         return
 
     print("\n---Borrow History---")
-    found = False
-    
-    for user in users:
-        borrowed =user.get("borrowed_books",[])
-        if borrowed:
-            found = True
-            print(f"\n User: {user['name']} (ID: {user['user_id']})")
-            for b in borrowed:
-                for book in books:
-                    if book["book_id"] == b["book_id"]:
-                        print(f" -  {book["title"]} by {book["author"]} | Borrowed: {b['borrow_date']} | Due: {b['due_date']}")
-    
-    if not found:
-        print("No books issued yet")
+    # found = False
+    for t in transactions:
+        user = next(u for u in users if u["user_id"] == t["user_id"])
+        book = next(b for b in books if b["book_id"] == t["book_id"])
+        
+        print(
+            f"{user['name']} | {book['title']} |"
+            f"Issued: {t["issue_date"]} |"
+            f"Returned: {t["return_date"]} |"
+            f"Status: {t["status"]}"
+        )
     
 #-------------------------------------------------------------------------------------------------------------------------------#
 
@@ -174,63 +171,36 @@ def delete_book(book_id):
     
 #-------------------------------------------------------------------------------------
 def return_book(user,book_id):
-    users_data = load_user(USERS_PATH)
-    users = users_data.get("users",[])
     books_data = load_user(BOOKS_PATH)
     books = books_data.get("books",[])
     transaction_data = load_user(TRANSACTION_PATH)
     transactions = transaction_data.get("transactions",[])
-    
-    for u in users:
-        if u["user_id"] == user["user_id"]:
-            current_user = u
-            break
-    else:
-        print("User not found")
-        return
-    
-    borrowed = current_user.get("borrowed_books",[])
-    fine = 0
-    for record in borrowed:
-        if record["book_id"] == book_id:
-            borrow_date = datetime.strptime(record["borrow_date"], "%Y-%m-%d").date()
-            today = date.today() 
-            due_date = datetime.strptime(record["due_date"], "%Y-%m-%d").date()
-
+    today = date.today()
+    for t in transactions:
+        if (
+            t["user_id"] == user["user_id"]
+            and t["book_id"] == book_id
+            and t["status"] == "ISSUED"
+        ):
+            due_date = datetime.strptime(t["due_date"], "%Y-%m-%d").date()
             overdue_days = (today - due_date).days
             fine = overdue_days * FINE_PER_DAY if overdue_days > 0 else 0
             
-           
+            t["return_date"] = str(today)
+            t["status"] = "RETURNED"
+            t["fine_amount"] = fine
+            t["fine_paid"] = fine == 0
             
-            # update book availability
             for book in books:
                 if book["book_id"] == book_id:
                     book["available_copies"] += 1
                     break
-                
-            borrowed.remove(record)
-            new_transaction_id = transactions[-1]["transaction_id"] + 1 if transactions else 1
-            # save transaction
-            transactions.append(
-                Transaction(
-                    transaction_id=new_transaction_id,
-                    user_id=user["user_id"],
-                    book_id=book_id,
-                    issue_date=str(borrow_date),
-                    due_date=str(due_date),
-                    return_date=str(today),
-                    status="RETURNED",
-                    fine_amount=fine,
-                    fine_paid=fine == 0
-                ).to_dict()
-            )
-    
-    save_json(USERS_PATH, users_data)
-    save_json(BOOKS_PATH, books_data)
-    save_json(TRANSACTION_PATH, transaction_data)
+            
+            save_json(BOOKS_PATH, books_data)
+            save_json(TRANSACTION_PATH, transaction_data)
 
-    print(f"Book returned successfully. Fine: ₹{fine}")
-    return
+            print(f"Book returned successfully. Fine: ₹{fine}")
+            return
 
 print("This book was borrowed by you.")
 
